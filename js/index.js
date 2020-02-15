6a97a3a53b027cfb6302218551b010c278bc1724
Amy M Haddad <amy.m.haddad@gmail.com>
2020-02-15 15:01:28 -0500

    Added multiple addEventListeners()

--- js/index.js
+++ js/index.js
@@ -1,98 +1,59 @@
 
-//QUestions
-// event.target.style.size = "50px"; -->Why doesn't this work?
-//When to use event 
-//When to use event.target
-//challenge looping through elements and applying the addEventListener to each on (ex 1)
+const navHeader = document.querySelector("nav");
+
+const navLinks = document.querySelectorAll(".nav-link");
+
+const logoHeader = document.querySelector(".logo-heading");
+
+const signUpButtons = document.querySelectorAll(".btn");
+
+const pickDestination = document.querySelector(".content-destination h2");
 
 
-// let allNavItems = document.querySelectorAll('a');
-
-// for (let i=0; i<=allNavItems.length; i++) {
-//     eachItem = allNavItems[i];
-// }
-
-
-
-//eventlistener 1 
-let navItem = document.querySelector('a');
-
-navItem.addEventListener("mouseover", function(event) {
-     event.target.style.color = "lightblue";
-});
-
-navItem.addEventListener("mouseleave", function(event) {
-    event.target.style.color = "black";
-});
-
-//eventlistener 2
-navItem.addEventListener("click", function(){
-    navItem.style.background = "gray";
-})
-
-navItem.addEventListener("mouseleave", function(){
-    navItem.style.background = "white";
-})
-
-//eventlistener 3 -->refactor using forEaach
-//https://gist.github.com/kuznetsovandrey76/39a1fba45c212c32415d000743211ab9
-let button = document.querySelectorAll('.destination .btn');
-
-for (let i=0; i<button.length; i++) {
-    let result = button[i];
-    result.addEventListener('click', function() {
-        alert("Get ready for a great trip!");
+navLinks.forEach(link => {
+    link.addEventListener("mouseenter", function(event) {
+        event.target.style.background = "lightblue";
     })
-}
-
-// eventlistener 4 -- unclear why this doesn't work
-
-// button.forEach((element => {
-//     element.addEventListener("click", function(){
-//         element.style.background = "pink";
-//     })
-// })
+} )
 
 
-// eventlistener 4 -- unclear why this doesn't work
-// let oneButton = document.querySelector('.btn');
-// oneButton.addEventListener("dbclick", function(){
-//    oneButton.style.background = "yellow";
-// })
-
-
-//Eventlistener 5 
-let contentChange = document.querySelectorAll(".text-content p");
-
-// for (let i=0; i<=contentChange.length; i++) {
-//     let change = contentChange[i];
-//     change.addEventListener("mouseover", function() {
-//         change.innerHTML = "Ready to travel!"
-//     })
-// }
-
-
-//Eventlistener 6 -- why does this not work?
-// let singleButton = document.querySelector('.btn');
-// singleButton.addEventListener("dblclick", function(event) {
-//     event.target.style.color = "red";
-//     // singleButton.style.color = "red";
-//     debugger
-// })
-
-
-//Event listener 7 
-const headers = document.querySelector("h2");
-
-headers.addEventListener("mouseenter", function() {
-    headers.style.color = "green";
-})
-
-headers.addEventListener("mouseleave", function() {
-    headers.style.color = "black";
+navLinks.forEach(link => {
+    link.addEventListener("mouseleave", event => {
+        event.target.style.background = "white";
+    })
 })
 
 
+navLinks.forEach(link => {
+    link.addEventListener("click", event => {
+        event.target.style.color = "orange";
+    })
+})
 
 
-debugger
+navHeader.addEventListener("dblclick", function(event) {
+    event.target.style.color = "green";
+})
+
+
+logoHeader.addEventListener("mouseover", function(event) {
+    event.target.style.width = "100px";
+})
+
+
+signUpButtons.forEach(button => {
+    button.addEventListener("click", event => {
+        event.target.style.background = "pink";
+    })
+})
+
+signUpButtons.forEach(button => {
+    button.addEventListener("mouseleave", event => {
+        event.target.style.background = "lightblue";
+    } )
+})
+
+
+pickDestination.addEventListener("mouseenter", function() {
+    alert("You'll have a great trip!")
+})
